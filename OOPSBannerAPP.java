c5acc2b1781716c2b08f470edda3c17d3e2a5963
Shouryaditya Dhanraj Singh <shouryaditya.ds@gmail.com>
2026-02-17 12:51:30 +0530

    feat: add UC3

--- OOPSBannerAPP.java
+++ OOPSBannerAPP.java
@@ -1,11 +1,11 @@
 public class OOPSBannerAPP {
 	public static void main(String[] args) {
-		System.out.println("   ***   "+"  "+"   ***   "+"  "+"******** "+"  "+" ******* ");
-		System.out.println(" **   ** "+"  "+" **   ** "+"  "+"*       *"+"  "+"*        ");
-		System.out.println("**     **"+"  "+"**     **"+"  "+"*       *"+"  "+"*        ");
-		System.out.println("**     **"+"  "+"**     **"+"  "+"******** "+"  "+" ******* ");
-		System.out.println("**     **"+"  "+"**     **"+"  "+"*        "+"  "+"        *");
-		System.out.println(" **   ** "+"  "+" **   ** "+"  "+"*        "+"  "+"        *");
-		System.out.println("   ***   "+"  "+"   ***   "+"  "+"*        "+"  "+" ******* ");
+		System.out.println(String.join("  ","   ***   ","   ***   ","******** "," ******* "));
+		System.out.println(String.join("  "," **   ** "," **   ** ","*       *","*        "));
+		System.out.println(String.join("  ","**     **","**     **","*       *","*        "));
+		System.out.println(String.join("  ","**     **","**     **","******** "," ******* "));
+		System.out.println(String.join("  ","**     **","**     **","*        ","        *"));
+		System.out.println(String.join("  "," **   ** "," **   ** ","*        ","        *"));
+		System.out.println(String.join("  ","   ***   ","   ***   ","*        "," ******* "));
 	}
 }
