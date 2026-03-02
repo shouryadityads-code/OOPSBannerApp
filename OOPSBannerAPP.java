c98bc672b23dc0d9a2288e41f222834e307a227d
Shouryaditya Dhanraj Singh <shouryaditya.ds@gmail.com>
2026-03-02 12:46:21 +0530

    feat: add UC7

--- OOPSBannerAPP.java
+++ OOPSBannerAPP.java
@@ -1,49 +1,72 @@
 public class OOPSBannerAPP {
-	public static String[] getOpattern() {
-		return new String[] {
-			"   ***   ",
-			" **   ** ",
-			"**     **",
-			"**     **",
-			"**     **",
-			" **   ** ",
-			"   ***   "
-		};
-	}
-	
-	public static String[] getPpattern() {
-		return new String[] {
-			"******** ",
-			"*       *",
-			"*       *",
-			"******** ",
-			"*        ",
-			"*        ",
-			"*        "
-		};
-	}
-	
-	public static String[] getSpattern() {
-		return new String[] {
-			" ******* ",
-			"*        ",
-			"*        ",
-			" ******* ",
-			"        *",
-			"        *",
-			" ******* "
-		};
-	}
-	
-	public static void main (String[] args) {
+	static class CharacterPattern {
+		private char character;
+        private String[] pattern;
 		
-		String[] Opattern = getOpattern();
-		String[] Ppattern = getPpattern();
-		String[] Spattern = getSpattern();
+		public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
 		
+		public char getCharacter() {
+            return character;
+        }
 		
-		for (int i = 0; i < Opattern.length; i++) {
-				System.out.println(Opattern[i] + "  " + Opattern[i] + "  " + Ppattern[i] + "  " + Spattern[i]);
+		public String[] getPattern() {
+            return pattern;
+        }
+    }
+	
+	public static CharacterPattern createOPattern() {
+        return new CharacterPattern('O', new String[]{
+                "   ***   ",
+                " **   ** ",
+                "**     **",
+                "**     **",
+                "**     **",
+                " **   ** ",
+                "   ***   "
+        });
+    }
+	
+	public static CharacterPattern createPPattern() {
+        return new CharacterPattern('P', new String[]{
+                "******** ",
+                "*       *",
+                "*       *",
+                "******** ",
+                "*        ",
+                "*        ",
+                "*        "
+        });
+    }
+	
+	public static CharacterPattern createSPattern() {
+        return new CharacterPattern('S', new String[]{
+                " ******* ",
+                "*        ",
+                "*        ",
+                " ******* ",
+                "        *",
+                "        *",
+                " ******* "
+        });
+    }
+	
+	public static void main(String[] args) {
+		CharacterPattern[] characters = {
+                createOPattern(),
+                createOPattern(),
+                createPPattern(),
+                createSPattern()
+        };
+		
+		for (int i = 0; i < 7; i++) {
+			for (int j = 0; j < characters.length; j++) { 
+				System.out.print(characters[j].getPattern()[i] + "  ");
+			}
+
+			System.out.println(); 
 		}
 	}
 }
